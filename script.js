86f40a909f0725bb31748783f2675d5109a6f583
iftekar2 <iftekar.hossain123@gmail.com>
2023-06-27 19:02:33 -0400

    Styling Pokemon Card

--- script.js
+++ script.js
@@ -54,15 +54,39 @@ const pokemonPopup = (data) => {
         <div class='buttonDiv'>
           <button id='closeButton onclick='closePopup()'>Close</button>
         </div>
-        <div class='popupPokemonCard'>
-          <div class='imageDiv'>
-            <img class='pokemonImage' src='${data.sprites.other.dream_world.front_default}' />
-          </div>
-          <div class='pokemonName'>
-            <h4>${data.name}</h4>
-          </div>
-          <div class='pokemonAbilities'>
-            <p>${abilitiesHtml}</p>
+        <div class='pokemonOuterSection'>
+          <div class='popupPokemonCard'>
+            <div class='nameAndHP'>
+              <div class='pokemonName'>
+                <h3>${data.name}</h3>
+              </div>
+              <div class='pokemonhp'>
+                <h4>${data.stats[0].stat.name}</h4>
+                <h3>${data.stats[0].base_stat}</h3>
+              </div>
+            </div>
+              <div class='pokemonImageDiv'>
+                <img class='pokemonImage' src='${data.sprites.other.dream_world.front_default}' />
+              </div>
+              <div class='pokemonAbilities'>
+                <p>${abilitiesHtml}</p>
+              </div>
+              <div class='powerDiv'>
+                <div class='pokemonPower'>
+                  <div class='attack'>
+                    <img src='./Image/attack.png' />
+                    <h4>${data.stats[1].base_stat}</h4>
+                  </div>
+                  <div class='shield'>
+                    <img src='./Image/shield.png' />
+                    <h4>${data.stats[2].base_stat}</h4>
+                  </div>
+                  <div class='speed'>
+                    <img src='./Image/speed.png' />
+                    <h4>${data.stats[5].base_stat}</h4>
+                  </div>
+                <div>
+              </div>
           </div>
         </div>
       </div>
